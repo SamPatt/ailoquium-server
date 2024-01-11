eb4d6a06d95c3a2de24d4d773542dbba28efe15b
SamPatt <git@sampatt.com>
2024-01-11 14:45:12 -0500

    Adds openai_api.py file to manage AI requests

---
+++ ailoquium/main_app/openai_api.py
@@ -0,0 +1,12 @@
+
+
+def send_message_to_openai(user_message):
+    # Logic to send the message to OpenAI API
+    # For example, using requests.post()
+    # return the API response
+    pass
+
+def modify_openai_response(api_response):
+    # Logic to modify the OpenAI API response
+    # return the modified response
+    pass
